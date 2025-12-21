c91a388b54c34dddff3b6828daad9cc8f3a7cb23
Shreyas Pals <71091209+Shreyas-Pals@users.noreply.github.com>
2025-12-21 17:37:50 +0530

    chore: remove comments in init.lua

--- init.lua
+++ init.lua
@@ -7,12 +7,8 @@ vim.keymap.set({ "n", "i" }, "<Left>", "<Nop>", opts)
 vim.keymap.set({ "n", "i" }, "<Right>", "<Nop>", opts)
 vim.g.mapleader = " "
 
--- Load Lazy and plugin specs
-
 -- Load your plugin list
 local plugins = require("plugins")
 
--- Setup lazy.nvim
 require("lazy").setup(plugins)
--- Your other config files
 require("keymaps")
