8c084c6a95fe3655807c2b60625d16dc60157783
Shreyas Pals <71091209+Shreyas-Pals@users.noreply.github.com>
2026-01-11 12:20:18 +0530

    fix: fix formatting, hide warnings.
    -warnings hide after 3 seconds
    -all indentation attributes set to 2 for all languages but lua (4)

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 -- Disable arrow keys
 local opts = { noremap = true, silent = true }
 
@@ -5,10 +7,29 @@ vim.keymap.set({ "n", "i" }, "<Up>", "<Nop>", opts)
 vim.keymap.set({ "n", "i" }, "<Down>", "<Nop>", opts)
 vim.keymap.set({ "n", "i" }, "<Left>", "<Nop>", opts)
 vim.keymap.set({ "n", "i" }, "<Right>", "<Nop>", opts)
-vim.g.mapleader = " "
 
--- Load your plugin list
+-- Terminal
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+-- vim.opt.formatoptions:remove({ "o", "r" })
+-- Fix C/C++ indentation (Tree-sitter takes control)
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "lua" },
+    callback = function()
+        vim.opt.tabstop = 4
+        vim.opt.shiftwidth = 4
+    end,
+})
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        vim.defer_fn(function()
+            pcall(vim.cmd, "Noice dismiss")
+        end, 3000)
+    end,
+})
+-- Plugins
 local plugins = require("plugins")
-
 require("lazy").setup(plugins)
+
+-- Extra keymaps
 require("keymaps")
