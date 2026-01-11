a7f6035fb88a517d6f14d2cec6bbcccada596c08
Shreyas Pals <71091209+Shreyas-Pals@users.noreply.github.com>
2026-01-11 12:22:40 +0530

    feat: keybind to run c/cpp files.
    -set global indentation = 2 whitespaces

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,14 @@
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+-- vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+-- vim.opt.cindent = false
 vim.opt.smartindent = true
+vim.opt.breakindent = true
+-- vim.opt.autoindent = true
 vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>r", function()
+    vim.cmd("w")
+    vim.cmd("split | terminal g++ -Wall -Wextra % -o main && ./main")
+end)
